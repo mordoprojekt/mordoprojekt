63a6070fb0300a000ab0effdf177bec7a81172d4
Adam Jedrzejewski <adamjedrzejewski@icloud.com>
2023-12-26 21:49:44 +0100

    refactor and format code

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,15 +1,15 @@
 use serde::Deserialize;
-use serenity::all::{CreateAttachment, CreateMessage};
-use std::fs;
-use std::process::exit;
-
-use serenity::async_trait;
+use serenity::all::{CreateAttachment, CreateMessage, GatewayIntents};
+use serenity::client::{Context, EventHandler};
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{
     CommandError, CommandResult, Configuration, StandardFramework,
 };
 use serenity::model::channel::Message;
-use serenity::prelude::*;
+use serenity::{async_trait, Client};
+use std::fs;
+use std::fs::File;
+use std::io::Read;
 
 #[derive(Deserialize)]
 struct Data {
@@ -37,33 +37,22 @@ impl serenity::prelude::TypeMapKey for Resources {
 fn read_config() -> Config {
     let config_file = "config.toml";
 
-    let contents = match fs::read_to_string(config_file) {
-        Ok(contents) => contents,
-        Err(_) => {
-            eprintln!("Could not read file {}", config_file);
-            exit(1);
-        }
-    };
-    let data: Data = match toml::from_str(&contents) {
-        Ok(contents) => contents,
-        Err(e) => {
-            eprintln!("Invalid config file: {}", e);
-            exit(1);
-        }
-    };
+    let contents = fs::read_to_string(config_file)
+        .expect(&format!("Config file: {} not found.", config_file));
 
-    data.config
+    toml::from_str::<Data>(&contents).expect("").config
 }
 
-use std::fs::File;
-use std::io::Read;
-
 fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
-    let mut f = File::open(&filename).expect("no file found");
-    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut f =
+        File::open(&filename).expect(&format!("File: {} not found", filename));
+    let metadata = fs::metadata(&filename)
+        .expect(&format!("Unable to read file: {}", filename));
     let mut buffer = vec![0; metadata.len() as usize];
-    f.read(&mut buffer)
-        .expect("get_file_as_byte_vec buffer overflow");
+    f.read(&mut buffer).expect(&format!(
+        "Unable to read file: {} buffer overflow",
+        filename
+    ));
 
     buffer
 }
@@ -71,12 +60,10 @@ fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
 // registers global app resources
 async fn create_resources(client: &mut Client) {
     let gimper = Attachment {
-        data: get_file_as_byte_vec(&String::from("img/gimper.jpg")),
+        data: get_file_as_byte_vec(&String::from("./img/gimper.jpg")),
         filename: String::from("gimper.jpg"),
     };
-    let resources = Resources {
-        gimper: gimper,
-    };
+    let resources = Resources { gimper: gimper };
 
     let mut data = client.data.write().await;
     data.insert::<Resources>(resources);
@@ -100,7 +87,8 @@ async fn main() {
     let framework = StandardFramework::new().group(&GENERAL_GROUP);
     framework.configure(Configuration::new().prefix(config.prefix));
 
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let intents =
+        GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .framework(framework)
@@ -109,7 +97,7 @@ async fn main() {
 
     // create app resources
     create_resources(&mut client).await;
-    
+
     // start listening for events by starting a single shard
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
@@ -128,8 +116,10 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 async fn gimper(ctx: &Context, msg: &Message) -> CommandResult {
     let data = ctx.data.read().await;
     let resources = data.get::<Resources>().unwrap();
-    let gimper_attachment =
-        CreateAttachment::bytes(resources.gimper.data.clone(), &resources.gimper.filename);
+    let gimper_attachment = CreateAttachment::bytes(
+        resources.gimper.data.clone(),
+        &resources.gimper.filename,
+    );
 
     let builder = CreateMessage::new().add_file(gimper_attachment);
     match msg.channel_id.send_message(&ctx.http, builder).await {
