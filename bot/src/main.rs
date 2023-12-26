e396f963feea3fe35df649a43d2a123ae261bddd
mrcoin0@gmail.com <13929715+mrozonn@users.noreply.github.com>
2023-12-26 18:37:48 +0100

    read config

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, Discord!!");
+use std::fs;
+use std::process::exit;
+use serenity::json::from_str;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Data{
+    config: Config
 }
+#[derive(Deserialize)]
+struct Config{
+    token: String
+}
+fn main() {
+    let configfile = "config.toml";
+    let contents = fs::read_to_string(configfile);
+    let readcontents = match contents{
+        Ok(contents) => contents,
+        Err(_) => {eprintln!("Could not read file");exit(1);}
+    };
+    let data:Data = match toml::from_str(&readcontents){
+        Ok(contents) => contents,
+        Err(e) => {eprintln!("cokolwiek innego {}", e);exit(1);}
+    };
+    println!("{}", data.config.token);
+}
+/* use std::env;
+
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, Configuration, CommandResult};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    framework.configure(Configuration::new().prefix("~")); // set the bot's prefix to "~"
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+ */
