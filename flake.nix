47bc425ded93e74a64a33723c2bc5d15f6a3f2a0
Adam Jędrzejewski <59581316+adamjedrzejewski@users.noreply.github.com>
2023-12-28 20:46:32 +0100

    nix cleanup (#12)
    nix files include only dev environment setup

--- flake.nix
+++ flake.nix
@@ -1,71 +1,19 @@
-# To get developer environment
+# To get developer environment run:
 # $ nix develop --impure
 {
   description = "Mordoprojekt discord bot!";
 
   inputs = {
-    # rust-overlay.url = "github:oxalica/rust-overlay";
-    # flake-utils.follows = "rust-overlay/flake-utils";
-    
     nixpkgs.url = "github:NixOS/nixpkgs/master";
-    # nixpkgs.follows = "rust-overlay/nixpkgs";
-    
     devenv.url = "github:cachix/devenv/v0.6.3";
-    
-    naersk.url = "github:nmattia/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, devenv, naersk, ... } @ inputs:
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
     let
-      cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
       supportedSystems = [ "aarch64-darwin" "x86_64-linux" ];
       forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
     {
-      overlay = final: prev: {
-        "${cargoToml.package.name}" = final.callPackage ./. { inherit naersk; };
-      };
-
-      packages = forEachSystem (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              self.overlay
-            ];
-          };
-        in
-        {
-          "${cargoToml.package.name}" = pkgs."${cargoToml.package.name}";
-        });
-
-      defaultPackage = forEachSystem (system: (import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      })."${cargoToml.package.name}");
-
-            checks = forEachSystem (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              self.overlay
-            ];
-          };
-        in
-        {
-          format = pkgs.runCommand "check-format"
-            {
-              buildInputs = with pkgs; [ rustfmt cargo ];
-            } ''
-            ${pkgs.rustfmt}/bin/cargo-fmt fmt --manifest-path ${./.}/Cargo.toml -- --check
-            ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
-            touch $out # it worked!
-          '';
-          "${cargoToml.package.name}" = pkgs."${cargoToml.package.name}";
-        });
-
       devShells = forEachSystem (system:
         let
             pkgs = import nixpkgs {};
