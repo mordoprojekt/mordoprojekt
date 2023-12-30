0de3d316ec9502e515428dc524c05d74e3c79a77
Adam Jędrzejewski <59581316+adamjedrzejewski@users.noreply.github.com>
2023-12-30 20:14:43 +0100

    Integrate OpenAI API (#15)
    Co-authored-by: mrozon <13929715+mrozonn@users.noreply.github.com>

--- src/commands.rs
+++ src/commands.rs
@@ -1,15 +1,19 @@
-use serenity::all::CreateAttachment;
 use crate::{Context, Error};
-use poise::serenity_prelude as serenity;
-use image::{ImageBuffer, RgbImage};
 
-const WIDTH:u32 = 256;
-const HEIGHT:u32 = 256;
+use poise::CreateReply;
+use serenity::all::{CreateAttachment, User};
+
+use image::{ImageBuffer, RgbImage};
+use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
+use openai_api_rs::v1::common::GPT3_5_TURBO;
+
+const WIDTH: u32 = 256;
+const HEIGHT: u32 = 256;
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn age(
     ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
+    #[description = "Selected user"] user: Option<User>,
 ) -> Result<(), Error> {
     let u = user.as_ref().unwrap_or_else(|| ctx.author());
     let response =
@@ -20,28 +24,61 @@ pub async fn age(
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn gimper(ctx: Context<'_>) -> Result<(), Error> {
-    let gimper = ctx.data().gimper.lock().await;
-    let gimper_attachment =
-        CreateAttachment::bytes(gimper.data.clone(), &gimper.filename);
-    let reply = poise::CreateReply::default().attachment(gimper_attachment);
+    let gimper_attachment = ctx.data().gimper_attachment.lock().await;
+    let attachment = CreateAttachment::bytes(
+        gimper_attachment.data.clone(),
+        &gimper_attachment.filename,
+    );
+    let reply = poise::CreateReply::default().attachment(attachment);
 
     ctx.send(reply).await?;
     Ok(())
 }
 
 #[poise::command(slash_command, prefix_command)]
-pub async fn paintdot(ctx: Context<'_>,
-                      #[description = "Współrzędna X"] a: u32,
-                      #[description = "Współrzędna Y"] b: u32
+pub async fn paintdot(
+    ctx: Context<'_>,
+    #[description = "Współrzędna X"] a: u32,
+    #[description = "Współrzędna Y"] b: u32,
 ) -> Result<(), Error> {
     let mut image: RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
-    *image.get_pixel_mut(a, b) = image::Rgb([255,255,255]);
+    *image.get_pixel_mut(a, b) = image::Rgb([255, 255, 255]);
     image.save("./img/output.png").unwrap();
     let image_data = std::fs::read("./img/output.png").unwrap();
-    let paintdot_attachment =
-        CreateAttachment::bytes(image_data, "output.png");
-    let reply = poise::CreateReply::default().attachment(paintdot_attachment);
+    let paintdot_attachment = CreateAttachment::bytes(image_data, "output.png");
+    let reply = CreateReply::default().attachment(paintdot_attachment);
 
     ctx.send(reply).await?;
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn gpt(
+    ctx: Context<'_>,
+    #[rest]
+    #[description = "prompt"]
+    prompt: String,
+) -> Result<(), Error> {
+    // TODO: using global singleton client for now, change to transient or scoped
+    let openai_client = ctx.data().openai_client.lock().await;
+    let req = ChatCompletionRequest::new(
+        GPT3_5_TURBO.to_string(),
+        vec![chat_completion::ChatCompletionMessage {
+            role: chat_completion::MessageRole::user,
+            content: prompt.to_string(),
+            name: None,
+            function_call: None,
+        }],
+    );
+
+    let result = openai_client.chat_completion(req)?;
+    let noresponse = String::from("no response");
+    let content = result.choices[0]
+        .message
+        .content
+        .to_owned()
+        .unwrap_or(noresponse);
+
+    ctx.reply(content).await?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 mod commands;
 
-use poise::serenity_prelude as serenity;
-use serenity::all::{CreateAttachment, GatewayIntents};
+use serenity::all::GatewayIntents;
+use serenity::async_trait;
 use serenity::client::EventHandler;
-use serenity::{async_trait, Client};
-use std::{fs, env};
 use std::fs::File;
 use std::io::Read;
+use std::process::exit;
+use std::{env, fs};
 use tokio::sync::Mutex;
+
+use openai_api_rs::v1::api::Client as OpenAiClient;
+use serenity::Client as SerenityClient;
+
+// names of env vars holding api keys and tokens
+const DISCORD_TOKEN: &'static str = "DISCORD_TOKEN";
+const OPENAI_API_KEY: &'static str = "OPENAI_API_KEY";
 
 struct Attachment {
     data: Vec<u8>,
@@ -15,64 +22,67 @@ struct Attachment {
 }
 
 pub struct Data {
-    gimper: Mutex<Attachment>,
-} // User data, which is stored and accessible in all command invocations
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
-    let mut f =
-        File::open(&filename).expect(&format!("File: {} not found", filename));
-    let metadata = fs::metadata(&filename)
-        .expect(&format!("Unable to read file: {}", filename));
-    let mut buffer = vec![0; metadata.len() as usize];
-    f.read(&mut buffer).expect(&format!(
-        "Unable to read file: {} buffer overflow",
-        filename
-    ));
-
-    buffer
+    openai_client: Mutex<OpenAiClient>,
+    gimper_attachment: Mutex<Attachment>,
 }
 
-// registers global app resources
-async fn create_resources() -> Attachment {
-    let gimper = Attachment {
-        data: get_file_as_byte_vec(&String::from("./img/gimper.jpg")),
-        filename: String::from("gimper.jpg"),
-    };
-    return gimper;
-}
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Context<'a> = poise::Context<'a, crate::Data, Error>;
 
 struct Handler;
-
 #[async_trait]
 impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
-    let token = env::var("DISCORD_TOKEN").expect("missing token");
-    let resources = Mutex::new(create_resources().await);
+    let discord_token =
+        env::var(DISCORD_TOKEN).expect("failed to read discord token");
+    let openai_api_key =
+        env::var(OPENAI_API_KEY).expect("failed to read openai key");
+
+    let gimper = match create_gimper_attachment() {
+        Ok(gimper) => gimper,
+        Err(e) => {
+            eprintln!("{}", e);
+            exit(1);
+        }
+    };
+
+    let gimper_attachment = Mutex::new(gimper);
+    let openai_client =
+        Mutex::new(OpenAiClient::new(openai_api_key.to_string()));
+
+    let app_data = Data {
+        gimper_attachment,
+        openai_client,
+    };
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![commands::age(), commands::gimper(), commands::paintdot()],
+            commands: vec![
+                commands::age(),
+                commands::gimper(),
+                commands::paintdot(),
+                commands::gpt(),
+            ],
             ..Default::default()
         })
-        .setup(|ctx, _ready, framework| {
+        .setup(|ctx, _, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(
                     ctx,
                     &framework.options().commands,
                 )
                 .await?;
-                Ok(Data { gimper: resources })
+
+                Ok(app_data)
             })
         })
         .build();
 
     let intents =
         GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(token, intents)
+    let mut client = SerenityClient::builder(discord_token, intents)
         .event_handler(Handler)
         .framework(framework)
         .await
@@ -82,4 +92,22 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
+}
+
+fn get_file_as_byte_vec(filename: &String) -> Result<Vec<u8>, Error> {
+    let mut f = File::open(&filename)?;
+    let metadata = fs::metadata(&filename)?;
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer)?;
+
+    Ok(buffer)
+}
+
+fn create_gimper_attachment() -> Result<Attachment, Error> {
+    let gimper = Attachment {
+        data: get_file_as_byte_vec(&String::from("./img/gimper.jpg"))?,
+        filename: String::from("gimper.jpg"),
+    };
+
+    Ok(gimper)
 }
