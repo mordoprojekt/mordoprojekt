781f02e50b411c2939c0e318466d3fc4b5b07976
mrozon <13929715+mrozonn@users.noreply.github.com>
2024-01-21 22:17:55 +0100

    applied suggestions
    Co-authored-by: Adam Jędrzejewski <59581316+adamjedrzejewski@users.noreply.github.com>

--- src/commands/thread.rs
+++ src/commands/thread.rs
@@ -2,15 +2,19 @@ use serenity::all::{ChannelType, CreateThread};
 use crate::{Context, Error};
 
 #[poise::command(slash_command, prefix_command)]
-pub async fn thread(ctx: Context<'_>, thread_name: String) -> Result<(), Error> {
+pub async fn thread(
+    ctx: Context<'_>,
+    #[description = "thread name"]
+    thread_name: String)
+-> Result<(), Error> {
     let handle = ctx.reply("Creating Thread...").await?;
     let thread = CreateThread::new(thread_name).kind(ChannelType::PublicThread);
     let channel = match ctx.guild_channel().await{
       Some(channel) => channel,
-        None => {
-            ctx.reply("Failed to get Channel").await?;
-            return Ok(())
-        }
+      None => {
+          ctx.reply("Failed to get Channel").await?;
+          return Ok(())
+      }
     };
     channel.create_thread(ctx, thread).await?;
     handle.delete(ctx).await?;
